379ee53a1a268b768a1766e881c0300b46933647
Marcus Sakoda <Jubhub@yahool.com>
2016-12-19 08:25:06 -0800

    Initial commit

---
+++ wait.js
@@ -0,0 +1,24 @@
+var daysUntilMyBirthday = 177;
+
+for (var i = daysUntilMyBirthday; i > 0; i--)
+{
+  if (i > 30)
+  {
+    console.log(i + " days left until my birthday.. so long.. :C");
+  }
+  else if (i < 5 && i > 1)
+  {
+    console.log(i + " DAYS LEFT UNTIL MY BIRTHDAY!");
+  }
+  else if (i === 1)
+  {
+    console.log(i + " DAY LEFT UNTIL MY BIRTHDAY!");
+  }
+  else if (i <= 30 && i > 5)
+  {
+    console.log("Only " + i + " days left until my birthday!");
+  }
+}
+console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*");
+console.log("♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪");
+console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*");
